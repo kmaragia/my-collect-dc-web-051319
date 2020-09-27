880ea3a778d4c9383da18f28eaff93825b115416
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-27 19:05:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(arr)
   index = 0 
   new_arr = []
   while arr.length > index
-    yield(arr[index])
+    new_arr << yield(arr[index])
     index += 1 
   end  
-  arr
+  new_arr
 end
